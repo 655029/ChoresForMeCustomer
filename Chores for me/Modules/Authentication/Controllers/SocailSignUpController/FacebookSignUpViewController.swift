92f4d0fd1a75408ecf5cb9a20c29ef3773043481
brightroots11 <92910737+brightroots11@users.noreply.github.com>
2022-03-06 00:12:38 +0530

    first commit

---
+++ Chores for me/Modules/Authentication/Controllers/SocailSignUpController/FacebookSignUpViewController.swift
@@ -0,0 +1,19 @@
+//
+//  FacebookSignUpViewController.swift
+//  Chores for me
+//
+//  Created by Ios Mac on 28/02/22.
+//
+
+import UIKit
+
+class FacebookSignUpViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+}
